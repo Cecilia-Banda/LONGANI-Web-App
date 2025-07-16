f878fa7c5e1bf12a87c3720c026a0f4632d752d9
Cecilia-Banda <c.banda@alustudent.com>
2025-07-16 17:40:12 +0200

    made services, added Axios provision

---
+++ Frontend/my-app-1.0/src/services/authService.js
@@ -0,0 +1,16 @@
+import API from '../utils/axios';
+
+export const registerUser = async (formData) => {
+  const res = await API.post('/auth/register', formData);
+  return res.data;
+};
+
+export const loginUser = async (formData) => {
+  const res = await API.post('/auth/login', formData);
+  return res.data;
+};
+
+export const getProfile = async () => {
+  const res = await API.get('/auth/profile');
+  return res.data;
+};
